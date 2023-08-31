38425f2ed2e9ca76f58887654000abec89bd0723
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-08-31 12:42:13 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "8f8cf732-11ef-4615-ba45-6ed91974d274",
+    "home_feed": [
+        {
+            "id": "24c6a8f8-9bc4-4078-8a58-beadc7028d55",
+            "artist_name": "group",
+            "home_carousel": [
+                {
+                    "id": "e047cab4-468d-4d7f-9e79-57ebd5180d11",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/64.png"
+                },
+                {
+                    "id": "82d7f368-746e-4135-a2d1-06ae360c6c9d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/268.png"
+                },
+                {
+                    "id": "c0c41dd1-cd08-4784-9106-18a7134c25a1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/38.png"
+                },
+                {
+                    "id": "51a6a52e-fd4c-465b-90f3-efec5d6d2be9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/264.png"
+                },
+                {
+                    "id": "3369a51f-d43e-48a7-8f27-cfbb4c22a759",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/368.png"
+                }
+            ],
+            "art_caption": "condition",
+            "art_description": "This director manage air factor thought care manager. Team about choose friend give director."
+        },
+        {
+            "id": "7ed7ef3f-fbe2-4008-8997-9f175ae00c43",
+            "artist_name": "picture",
+            "home_carousel": [
+                {
+                    "id": "4800973c-5d1b-4f2a-9fd1-69a2324daf7c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/108.png"
+                },
+                {
+                    "id": "0d071a8d-3d66-4430-a3a7-c4d40faf6a84",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/265.png"
+                },
+                {
+                    "id": "b26ab9a7-5bd5-498f-8c81-44281a50780f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/200.png"
+                },
+                {
+                    "id": "23e072f3-4901-40ad-aea9-3b59303d66e7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/73.png"
+                },
+                {
+                    "id": "8f112c43-beb1-4f29-af4b-c7e7dfcee206",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/220.png"
+                }
+            ],
+            "art_caption": "serve",
+            "art_description": "Mr she hit option response sense. Probably study close church traditional each."
+        },
+        {
+            "id": "4b209419-4d11-4735-917a-78fab55ba1d6",
+            "artist_name": "human",
+            "home_carousel": [
+                {
+                    "id": "ea66b837-33c0-47a3-9ceb-fc76ab2e1ab4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/127.png"
+                },
+                {
+                    "id": "f8bfe4dc-6510-43f7-bff3-fda4dcf2dbdb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/13.png"
+                },
+                {
+                    "id": "fee9adb3-2c81-4543-a12e-466addecec86",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/136.png"
+                },
+                {
+                    "id": "c2cebc77-eabb-400b-93ce-811b15b6d60d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/83.png"
+                },
+                {
+                    "id": "5b653af1-2b08-4318-a156-017348cc5613",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/240.png"
+                }
+            ],
+            "art_caption": "concern",
+            "art_description": "Central good why experience. Support understand travel us data. Concern network upon wind look opportunity."
+        },
+        {
+            "id": "b6a44324-f9c5-48eb-9e7f-2cdeecb78a19",
+            "artist_name": "herself",
+            "home_carousel": [
+                {
+                    "id": "45d868a5-8dba-4282-8eff-554df4b795f1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/270.png"
+                },
+                {
+                    "id": "acbfab7a-1c5a-4fe3-8d39-3c81188dc57b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/63.png"
+                },
+                {
+                    "id": "47cee845-3600-490b-bd3f-42c295d41a9a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/210.png"
+                },
+                {
+                    "id": "ca73adf1-b418-4593-a301-13a38dfc71be",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/91.png"
+                },
+                {
+                    "id": "c81d09ea-7545-44f6-a092-e9a21aa0e107",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/117.png"
+                }
+            ],
+            "art_caption": "good",
+            "art_description": "Deal foot bank early investment."
+        },
+        {
+            "id": "aa0d6d30-dd54-4f55-b833-83cd14924d7b",
+            "artist_name": "usually",
+            "home_carousel": [
+                {
+                    "id": "1d1155d7-b57b-4172-983b-3ccf82513aa8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/344.png"
+                },
+                {
+                    "id": "ac831171-a722-45e4-8380-190c28c7ec22",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/190.png"
+                },
+                {
+                    "id": "129ea628-971d-425c-8c5e-c6906e5f6b23",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/99.png"
+                },
+                {
+                    "id": "129e3c70-f24b-486d-ad10-eafd2727e59a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/107.png"
+                },
+                {
+                    "id": "7330225d-32b3-4fbb-9a73-3f6c21924171",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/92.png"
+                }
+            ],
+            "art_caption": "agency",
+            "art_description": "Although anyone check official life while. Above author try court design world."
+        }
+    ]
+}
